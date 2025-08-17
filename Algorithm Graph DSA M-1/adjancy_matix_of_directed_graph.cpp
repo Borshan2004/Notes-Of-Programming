1ca5d3bbfe25590e3c3163c23f614ee938297836
Borshan2004 <borshanpurification42@gmail.com>
2025-08-17 17:22:48 +0600

    algorithm

---
+++ Algorithm Graph DSA M-1/adjancy_matix_of_directed_graph.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+// n and e are node and eges 
+int n,e;
+cin>>n>>e;
+
+int adjancy_m [n][n];
+
+// rule of use memset
+// (who's value is store, what is store,size of the store)
+
+memset(adjancy_m,0,sizeof(adjancy_m));
+
+
+// make the digonal of the matrix 1 as the rule of adjancy matrix
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(i==j){
+            adjancy_m [i][j]=1;
+        }
+    }
+}
+
+// take the input of the conection and make the adjancy matrix
+//we have to run the loop accorfing to the edges
+for(int i=0;i<e;i++){
+
+    int a,b;
+    cin>>a>>b;
+
+    adjancy_m [a][b]=1;
+
+}
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<adjancy_m[i][j]<<" ";
+    }
+    cout<<endl;
+}
+
+
+
+
+
+
+
+return 0;
+}
